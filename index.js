54060a4a031afcf2e80f1ac8352549463730baf9
Oliver Judge <oliver.judge@guestline.com>
2022-11-30 16:40:28 +0000

    added if statement, yet to test it

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ let getComputerChoice = () => {
 
 getComputerChoice();
 
-let playerSelection = window.prompt("pick a number between 1 and 3");
+
+let playerSelection = window.prompt("Type a number between 1 and 3");
 
 console.log(playerSelection);
+
+if (playerSelection == 1 && getComputerChoice == 2 || playerSelection == 2 && getComputerChoice == 1 || playerSelection == 3 && getComputerChoice == 2) {
+    return document.getElementById("result").innerHTML = "You Win!";
+} else {
+    return document.getElementById("result").innerHTML = "You Loose!";
+}
