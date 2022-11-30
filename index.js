f21061faa0e902fae3945810acdde71aeea4db28
Oliver Judge <oliver.judge@guestline.com>
2022-11-30 16:08:31 +0000

    saved again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const generateRandomNUmberBetween1and3 = () => {
+const generateRandomNumberBetween1and3 = () => {
    let random = Math.random();
    console.log(Math.floor(random*3 + 1));
 }
 
-generateRandomNUmberBetween1and3();
+generateRandomNumberBetween1and3();
